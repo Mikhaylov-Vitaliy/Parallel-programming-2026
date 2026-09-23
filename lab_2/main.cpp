7a39b397b8a67647dce9f5b7e977d409ada2be73
Mikhaylov-Vitaliy <vmihailov2007@mail.ru>
2026-09-23 13:40:52 +0400

    Update main.cpp

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -74,7 +74,7 @@ int main() {
 	if (choice == 1) {
 		std::print("Введите размер матрицы (N): ");
 		int customSize = 0;
-		if (!(std::cin >> customSize) || customSize <= 0 || customSize > 1000) {
+		if (!(std::cin >> customSize) || customSize <= 0 || customSize > 2000) {
 			std::println(std::cerr, "Ошибка: Некорректный размер матрицы!");
 			return 1;
 		}
